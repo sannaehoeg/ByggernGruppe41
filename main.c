081e32a0b070c3d53725725f6c59bfe2acaf3501
Sanna Elida Høeg <sannaeh@ntnu.no>
2026-08-31 16:57:21 +0200

    exercise 1.6 toggling PC0

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * Byggern41.c
+ *
+ * Created: 31.08.2026 14:22:18
+ * Author : sannaeh
+ */ 
+
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+	DDRC |= (1<<PC0);
+	
+    while (1) 
+    {
+		PORTC ^= (1<<PC0);
+		_delay_ms(100);
+    }
+}
+
